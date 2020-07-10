01e78f34d06211f0710162011e4ab4af9760c104
Ashley Keller <kochx315@gmail.com>
2020-07-10 09:03:38 -0500

    Add home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResearchRouteComponent } from './research-route/research-route.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'research-a-route', pathMatch:'full'},
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'research-a-route', component: ResearchRouteComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { ResearchRouteComponent } from './research-route/research-route.component';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResearchRouteComponent
+    ResearchRouteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
